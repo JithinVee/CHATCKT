729d745123b2cc3e3b98da96017392ce4896a7af
aswinmshopalyst <aswin.m@shopalyst.com>
2024-12-01 22:00:34 +0530

    adding ui

---
+++ ui/my-react-app/src/Chatbot.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Chatbot.css"; // Link to the CSS file
+
+const Chatbot = () => {
+  return (
+    <div className="chatbot">
+      Chatbot
+    </div>
+  );
+};
+
+export default Chatbot;
