b57e4acf24f85dbf69dc50a1112875d6c5975e04
Jithin V <jithin@shopalyst.com>
2024-12-02 01:37:28 +0530

    api cors add

--- llm_api/app.py
+++ llm_api/app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from routes.llm_routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 # Initialize the FastAPI app
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include the query controller routes
 app.include_router(router)
